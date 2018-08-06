d62b62a577a56bdce46250921d3316e316996657
Jasdeep Aulakh <jasdeep.aulakh@cjcit.com>
2018-08-06 16:35:36 +0100

    Added POST method

--- src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
@@ -7,8 +7,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.uk.jasdeepaulakh.resttest.entity.Product;
@@ -51,4 +55,10 @@ public class ProductController {
 		return product;
 		
 	}
+	
+	@PostMapping("/addproduct")
+	public @ResponseBody ResponseEntity<String> addProduct(){
+		return new ResponseEntity<String>("POST Response", HttpStatus.OK);
+	}
+
 }
