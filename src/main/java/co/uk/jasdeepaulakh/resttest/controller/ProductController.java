cbc54bc348793fa0efa1cab630ea47761b868857
jasdeep.aulakh <jasdeep.aulakh@LON-TEC-24J.cjcit.com>
2018-08-13 15:25:52 +0100

    Added getAllProducts

--- src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -30,11 +32,14 @@ public class ProductController {
 		props.setProperty("password", "514d109b01329f487e1948e488276be6943069bc2e61e9a51d682856b1f6f322");
 		props.setProperty("ssl","true");
 		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
 		try {
 			Class.forName("org.postgresql.Driver");
-			Connection conn = DriverManager.getConnection(url,props);
+			conn = DriverManager.getConnection(url,props);
 			System.out.println(conn.isClosed());
-			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM products WHERE id=?");
+			stmt = conn.prepareStatement("SELECT * FROM products WHERE id=?");
 			stmt.setInt(1, id);
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
@@ -48,12 +53,78 @@ public class ProductController {
 			}
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println(e.getMessage());
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		
-		
+
 		return product;
 		
 	}
+	
+	@RequestMapping("/getproducts")
+	public List<Product> getProduct() {
+		
+		List<Product> productList = new ArrayList<>();
+		
+		String url = "jdbc:postgresql://ec2-23-21-129-50.compute-1.amazonaws.com:5432/d8jm11dddo7ds2?sslmode=require";
+		Properties props = new Properties();
+		props.setProperty("user", "grlahjqfhsjkkl");
+		props.setProperty("password", "514d109b01329f487e1948e488276be6943069bc2e61e9a51d682856b1f6f322");
+		props.setProperty("ssl","true");
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(url,props);
+			stmt = conn.prepareStatement("SELECT * FROM products");
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				int id2 = rs.getInt("id");
+				String name = rs.getString("name");
+				String desc = rs.getString("description");
+				double price = rs.getDouble("price");
+				
+				productList.add(new Product(id2, name, desc, price));
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		return productList;
+		
+	}
+	
 	
 	@PostMapping("/addproduct")
 	public void addProduct(@RequestBody Map<String, String> body){
@@ -68,23 +139,36 @@ public class ProductController {
 		props.setProperty("password", "514d109b01329f487e1948e488276be6943069bc2e61e9a51d682856b1f6f322");
 		props.setProperty("ssl","true");
 		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
 		try {
 			Class.forName("org.postgresql.Driver");
-			Connection conn = DriverManager.getConnection(url,props);
-			PreparedStatement stmt = conn.prepareStatement("INSERT INTO products VALUES (DEFAULT, ?, ?, ?);");
+			conn = DriverManager.getConnection(url,props);
+			stmt = conn.prepareStatement("INSERT INTO products VALUES (DEFAULT, ?, ?, ?);");
 			stmt.setString(1,name);
 			stmt.setString(2, description);
 			stmt.setDouble(3, price);
 			stmt.executeUpdate();
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println(e.getMessage());
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-	
-		
-		
-		
-		
-		
+
 	}
 
 }
