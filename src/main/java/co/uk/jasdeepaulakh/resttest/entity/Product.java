359f947da7c4749520637483c062169c7d5800ba
jasdeep.aulakh <jasdeep.aulakh@LON-TEC-24J.cjcit.com>
2018-08-14 10:30:27 +0100

    Added Table name to Product POJO

--- src/main/java/co/uk/jasdeepaulakh/resttest/entity/Product.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/entity/Product.java
@@ -4,13 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="products")
 public class Product {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	
 	private String name;
 	private String description;
 	private double price;
