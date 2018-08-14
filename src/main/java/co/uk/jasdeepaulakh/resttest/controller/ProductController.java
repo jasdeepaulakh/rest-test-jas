a782317fa5be402a09c8db6e4225ab3e79be37b5
jasdeep.aulakh <jasdeep.aulakh@LON-TEC-24J.cjcit.com>
2018-08-14 10:38:51 +0100

    Added JDBC RequestMapping method back in

--- src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/controller/ProductController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.uk.jasdeepaulakh.resttest.entity.Product;
@@ -32,6 +33,59 @@ public class ProductController {
 	public Optional<Product> getProduct(@PathVariable String id) {
 		int productId = Integer.parseInt(id);
 		return productRepository.findById(productId);
+		
+	}
+	
+	@RequestMapping("/product")
+	public Product getProduct(@RequestParam(value="id") int id) {
+		
+		Product product = null;
+		
+		String url = "jdbc:postgresql://ec2-23-21-129-50.compute-1.amazonaws.com:5432/d8jm11dddo7ds2?sslmode=require";
+		Properties props = new Properties();
+		props.setProperty("user", "grlahjqfhsjkkl");
+		props.setProperty("password", "514d109b01329f487e1948e488276be6943069bc2e61e9a51d682856b1f6f322");
+		props.setProperty("ssl","true");
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(url,props);
+			System.out.println(conn.isClosed());
+			stmt = conn.prepareStatement("SELECT * FROM products WHERE id=?");
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				int id2 = rs.getInt("id");
+				String name = rs.getString("name");
+				String desc = rs.getString("description");
+				double price = rs.getDouble("price");
+				System.out.println(name + desc);
+				
+				product = new Product(id2, name, desc, price);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return product;
 		
 	}
 	
