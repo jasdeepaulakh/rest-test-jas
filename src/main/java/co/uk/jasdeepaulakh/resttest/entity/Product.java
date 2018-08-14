c1e88f37c53c396a84854b23d669272220e2cc54
jasdeep.aulakh <jasdeep.aulakh@LON-TEC-24J.cjcit.com>
2018-08-14 10:35:58 +0100

    Changed Id from Auto generate to Identity

--- src/main/java/co/uk/jasdeepaulakh/resttest/entity/Product.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/entity/Product.java
@@ -11,9 +11,8 @@ import javax.persistence.Table;
 public class Product {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	
 	private String name;
 	private String description;
 	private double price;
