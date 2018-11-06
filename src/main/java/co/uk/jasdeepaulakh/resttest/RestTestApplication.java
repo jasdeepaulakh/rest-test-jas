92cf2ebbb17223447db343ead50f7aa0d27666c2
jasdeep.aulakh <jasdeep.aulakh@fdmgroup.com>
2018-11-06 10:19:56 +0000

    Added addCorsMapping to RestTestApplication

--- src/main/java/co/uk/jasdeepaulakh/resttest/RestTestApplication.java
+++ src/main/java/co/uk/jasdeepaulakh/resttest/RestTestApplication.java
@@ -9,4 +9,14 @@ public class RestTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestTestApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 }
